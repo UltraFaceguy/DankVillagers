1964a844cc862011e88b9da5be756e952b0b832f
Kyle Wolfgang Kattrein <kattreinkyle@gmail.com>
2016-05-20 00:44:05 -0400

    Initial commit of a rewrite of various horrible spawner plugins. Let's do it right this time people! It isn't even hard!

---
+++ src/com/dankspawners/src/main/java/com.faceguy.dankspawners/DankSpawnerCommands.java
@@ -0,0 +1,71 @@
+package com.faceguy.dankspawners;
+
+import java.util.ArrayList;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class DankSpawnerCommands implements CommandExecutor {
+    public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("ds")) {
+            if (args.length == 0) {
+                s.sendMessage(ChatColor.GREEN + "Usage: /ds give [player] [entity] (amount)");
+                return true;
+            }
+            if (args.length < 3) {
+                s.sendMessage(ChatColor.GREEN + "Usage: /ds " + ChatColor.GRAY + "give [player] [entity] (amount)");
+                return true;
+            }
+            if (!args[0].equalsIgnoreCase("give")) {
+                s.sendMessage(ChatColor.GREEN + "Usage: /ds " + ChatColor.GRAY + "give [player] [entity] (amount)");
+                return true;
+            }
+            if (!s.hasPermission("dankspawners.give")) {
+                s.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+                return true;
+            }
+            Player player = Bukkit.getPlayer(args[1]);
+            if (player == null) {
+                s.sendMessage(ChatColor.RED + "Invalid player name!");
+                return true;
+            }
+            EntityType type = EntityType.valueOf(args[2].toUpperCase());
+            if (type == null) {
+                s.sendMessage(ChatColor.RED + "Invalid entity name!");
+                return true;
+            }
+            int amount = 1;
+            if (args[3] != null) {
+                amount = Integer.valueOf(args[3]);
+            }
+            giveSpawner(player, type, amount);
+            s.sendMessage(ChatColor.GREEN + "Successfully gave " + player + " " + amount + " " + type + " spawners!");
+        }
+        return true;
+    }
+
+    public void giveSpawner(Player player, EntityType type, int amount) {
+        ItemStack item = getSpawnerItem(amount, type);
+        player.getInventory().addItem(item);
+    }
+
+    public static ItemStack getSpawnerItem(int amount, EntityType type) {
+        ItemStack item = new ItemStack(Material.MOB_SPAWNER, amount);
+        ArrayList<String> lore = new ArrayList<>();
+        String loreString = type.toString();
+        loreString = String.valueOf(loreString.substring(0, 1).toUpperCase()) + loreString.substring(1).toLowerCase();
+        loreString = String.valueOf(loreString) + " Spawner";
+        lore.add(loreString);
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+        return item;
+    }
+}
---
+++ src/com/dankspawners/src/main/java/com.faceguy.dankspawners/DankSpawnerListeners.java
@@ -0,0 +1,49 @@
+package com.faceguy.dankspawners;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class DankSpawnerListeners implements Listener {
+    @EventHandler (priority = EventPriority.MONITOR)
+    public void onPlayerMineSpawner(BlockBreakEvent evt) {
+        if (!evt.getBlock().getType().equals(Material.MOB_SPAWNER)) {
+            return;
+        }
+        if (!evt.getPlayer().hasPermission("dankspawners.mine")) {
+            return;
+        }
+        if (!evt.getPlayer().getEquipment().getItemInMainHand().getEnchantments().containsKey(Enchantment.SILK_TOUCH)) {
+            return;
+        }
+        evt.setExpToDrop(0);
+        evt.getBlock().getDrops().clear();
+        CreatureSpawner s = (CreatureSpawner)evt.getBlock().getState();
+        ItemStack item = DankSpawnerCommands.getSpawnerItem(1, s.getSpawnedType());
+        evt.getBlock().getDrops().add(item);
+    }
+
+    @EventHandler
+    public static void onSpawnerPlaced(BlockPlaceEvent evt) {
+        ItemStack item = evt.getItemInHand();
+        if (item == null) {
+            return;
+        }
+        if (item.getType().equals(Material.MOB_SPAWNER)) {
+            String sType = item.getItemMeta().getLore() == null ? "Pig Spawner" : item.getItemMeta().getLore().get(0);
+            Block setBlock = evt.getBlock();
+            setBlock.setType(Material.MOB_SPAWNER);
+            CreatureSpawner s = (CreatureSpawner)setBlock.getState();
+            s.setSpawnedType(EntityType.valueOf(sType.split(" ")[0].toUpperCase()));
+        }
+    }
+}
+
